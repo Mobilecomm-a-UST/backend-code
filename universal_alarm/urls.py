755b901bb691fcd369a55041e759f591c65c2827
PrernaGoyal01 <Prerna.PramodKumar@ust.com>
2026-04-28 12:30:40 +0530

    HR Trend and Universal Alarm

--- universal_alarm/urls.py
+++ universal_alarm/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from universal_alarm.views import upload_5g_log_file,upload_4g_log_file
+from universal_alarm.views import *
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -10,7 +10,9 @@ urlpatterns = [
     # path('upload_4g/', upload_log_file, ),
     path('upload_4g/', upload_4g_log_file, ),
     path('upload_5g/', upload_5g_log_file, ),
-   
-    
-         
+    path('upload_site/', upload_site_list, ),
+    path('delete_site/', delete_site, ),
+    path('get_site/', get_sites, ),
+
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
